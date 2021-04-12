eb8f929e29b58366a390527e05fabef7b0754e5e
Anders Valind <anders.valind@med.lu.se>
2021-04-12 17:16:33 +0000

    Support for multiple categories of sounds & pictures.

--- farsbot.py
+++ farsbot.py
@@ -9,11 +9,23 @@ from discord.ext import commands
 
 logging.basicConfig(filename="farsbot.log", level=logging.DEBUG)
 
+soundbase_cfg = {
+    "fars": "sounds", #!farsljud fars eller !farsljud slumpar fram ljudklipp ur sounds
+    "mille": "mille", #!farsljud mille slumpar fram ur mille etc.
+    "vinslov": "vinslov"
+}
+
+# samma princip.
+imgbase_cfg = {
+    "fars": "fars",
+    "nils": "nils"
+}
+
 def sample_soundclip(soundbase="sounds", ending=".wav"):
     return random.choice(glob.glob("{}/*{}".format(soundbase, ending)))
 
-def get_random_fars_image():
-    return random.choice(glob.glob("{}/*{}".format("fars", ".jpg")))
+def get_random_fars_image(imgbase="fars"):
+    return random.choice(glob.glob("{}/*{}".format(imgbase, ".jpg")))
 
 def load_token(filename="token.json"):
     with open(filename) as handle:
@@ -25,14 +37,23 @@ class FarsBot(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def farsljud(self, ctx):
+    async def farsljud(self, ctx, category="fars"):
+        if category in soundbase_cfg:
+            sbase = soundbase_cfg[category]
+        else:
+            sbase = "fars"
+
         client = self.bot.voice_clients[0]
-        src = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(sample_soundclip()))
+        src = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(sample_soundclip(soundbase=sbase)))
         client.play(src, after=lambda e: print('Player error: %s' % e) if e else None)
 
     @commands.command()
-    async def fars(self, ctx):
-        await ctx.channel.send(file=discord.File(get_random_fars_image()))
+    async def fars(self, ctx, category="fars"):
+        if category in imgbase_cfg:
+            imbase = imgbase_cfg[category]
+        else:
+            imbase = "fars"
+        await ctx.channel.send(file=discord.File(get_random_fars_image(imgbase=imbase)))
 
     @commands.command()
     async def join(self, ctx, *, channel: discord.VoiceChannel):
