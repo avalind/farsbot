a56e08069199785470f435976167105609df0f5c
Kristian Valind <5838803+kristianvalind@users.noreply.github.com>
2024-11-01 13:21:47 +0000

    Detta borde verkligen funka

--- farsbot.py
+++ farsbot.py
@@ -274,9 +274,8 @@ def journal_callback():
     for entry in j:
         asyncio.ensure_future(process(entry))
 
-join_matcher = re.compile('(?:ZDOID from )(.*)(?: : )(?:.*:1\\b)')
-print(join_matcher)
-death_matcher = re.compile('(?:ZDOID from )(.*)(?: : )(?:.*0:0)')
+join_matcher = re.compile(r'(?:ZDOID from )(.*)(?: : )(?:.*:1\b)')
+death_matcher = re.compile(r'(?:ZDOID from )(.*)(?: : )(?:.*0:0)')
 
 async def process(event):
     textline = str(event['MESSAGE'])
