d07216646e9e46fe82ef45f0926c0d2014458a72
Kristian Valind <5838803+kristianvalind@users.noreply.github.com>
2024-11-13 12:46:39 +0000

    Hantera meddelanden?

--- farsbot.py
+++ farsbot.py
@@ -330,6 +330,14 @@ bot = commands.Bot(
     intents=i,
 )
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 
 async def main():
     t = load_token()
